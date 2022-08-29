f6920d8f9d5754f901e23c0db71d00216461822f
Mani <manivannan33@gmail.com>
2022-08-29 08:31:34 -0400

    Added and updated HdfReader

--- raw/HdfReader.m
+++ raw/HdfReader.m
@@ -2,7 +2,7 @@ classdef (Sealed) HdfReader
     properties
         filename
         chan_name
-        unit % units for each signal (e.g. uV, % etc)       
+        units % units for each signal (e.g. uV, % etc)       
         fs % sampling rate (Hz)
         nbsamples % total number of samples per signal
         t0 % hardware counter value corresponding to the first data point of signals
@@ -11,26 +11,20 @@ classdef (Sealed) HdfReader
     methods
         function self = HdfReader(filename)
             % This is a constructor. Read basic info for later use.
-            self.chan_name = h5readatt(filename, '/', 'chan_name');
+            fields = {'chan_name','units','fs','nbsamples','t0'};
+            for iField = 1:length(fields)
+                cf = fields{iField};
+                self.(cf) = h5readatt(filename, '/', cf);
+            end
             self.filename = filename;
-            % Pull meta data
-            self.signal_names = h5readatt(filename,'/','SignalLabels');
-            self.signal_units = h5readatt(filename, '/', 'PhysicalDimensions');
-            self.Fs = h5readatt(filename,'/','Fs');
-            self.nbsamples_per_sig = h5readatt(filename,'/','NbsamplesPerSignal');
-            sdate = h5readatt(filename,'/','StartDate');
-            stime = h5readatt(filename,'/','StartTime');
-            tstr = strcat(sdate, '.', stime);
-            self.t0_datenum = datenum(tstr, 'dd.mm.yy.HH.MM.SS');
         end
-        function d = get_signal(self, signal_name, start_index, nsamples)
+        function d = get_signal(self, start_index, nsamples)
             % Get signal data.
-            % d = get_signal(hReader, signal_name, start_index, nsamples)
+            % d = get_signal(hReader, start_index, nsamples)
             %
             % Inputs:
             %   hReader - reader object (e.g: hReader =
-            %   HdfReader('H:\EEG\test.h5'))
-            %   signal_name - name of signal. e.g: 'Fz'
+            %   HdfReader('H:\EEG\test.h5'))            %  
             %   start_index - starting position index of the signal. e.g: 1001
             %                 nsamples - number of samples needed. e.g: 10000
             % Output:
@@ -40,17 +34,15 @@ classdef (Sealed) HdfReader
             %   d = get_signal(hReader, 'Fz', 1001, 10000);
             %   d = hReader.get_signal('Fz',1001, 10000);
             % To get all available data:
-            %   d = hReader.get_signal('Fz');
+            %   d = hReader.get_signal();
 
-            % Get data for a given signal
-            assert(any(strcmp(self.signal_names, signal_name)),...
-                'Requested signal does not exits in the data file')
-            group_loc = strcat('/', signal_name);
+            % Get data 
+            group_loc = strcat('/', self.chan_name);
             col_num = 1; % Data is in column vector form
 
             % If use wants all the availble data:
             if nargin < 3
-                nsamples = self.nbsamples_per_sig;
+                nsamples = self.nbsamples;
                 start_index = 1;
             end
             d = h5read(self.filename, group_loc, [start_index, col_num], [nsamples,col_num]);
